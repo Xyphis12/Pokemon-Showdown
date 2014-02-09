e9ba2536c46d9ae5faec0ba8ffbaec51f9e144f2
dtalley11 <talley_daniel@yahoo.com>
2014-02-08 20:04:55 -0500

    Added friend functinality from frost

--- commands.js
+++ commands.js
@@ -17,6 +17,141 @@ const MAX_REASON_LENGTH = 300;
 
 var commands = exports.commands = {
 
+	// Friends lists, from frost 
+	friends: function(target, room, user, connection) {
+		var data = fs.readFileSync('config/friends.csv','utf8')
+			var match = false;
+			var friends = '';
+			var row = (''+data).split("\n");
+			for (var i = 0; i < row.length; i++) {
+				if (!row[i]) continue;
+				var parts = row[i].split(",");
+				var userid = toUserid(parts[0]);
+				if (user.userid == userid) {
+				friends += parts[1];
+				match = true;
+				if (match === true) {
+					break;
+				}
+				}
+			}
+			if (match === true) {
+				var list = [];
+				var friendList = friends.split(' ');
+				for (var i = 0; i < friendList.length; i++) {
+					if(Users.get(friendList[i])) {
+						if(Users.get(friendList[i]).connected) {
+							list.push(friendList[i]);
+						}
+					}
+				}
+				if (list[0] === undefined) {
+					return this.sendReply('You have no online friends.');
+				}
+				var buttons = '';
+				for (var i = 0; i < list.length; i++) {
+					buttons = buttons + '<button name = "openUser" value = "' + Users.get(list[i]).userid + '">' + Users.get(list[i]).name + '</button>';
+				}
+				this.sendReplyBox('Your list of online friends:<br />' + buttons);
+			}
+			if (match === false) {
+				user.send('You have no friends to show.');
+			}
+		},
+	 
+	addfriend: function(target, room, user, connection) {
+		if(!target) return this.parse('/help addfriend');
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!targetUser) {
+			return this.sendReply('User '+this.targetUsername+' not found.');
+		}
+		if (targetUser.userid === user.userid) {
+			return this.sendReply('Are you really trying to friend yourself?');
+		}
+		var data = fs.readFileSync('config/friends.csv','utf8')
+		var match = false;
+		var line = '';
+		var row = (''+data).split("\n");
+		for (var i = row.length; i > -1; i--) {
+			if (!row[i]) continue;
+			var parts = row[i].split(",");
+			var userid = toUserid(parts[0]);
+			if (user.userid == userid) {
+				match = true;
+			}
+			if (match === true) {
+				line = line + row[i];
+				var individuals = parts[1].split(" ");
+				for (var i = 0; i < individuals.length; i++) {
+					if (individuals[i] === targetUser.userid) {
+						return connection.send('This user is already in your friends list.');
+					}
+				}
+				break;
+			}
+		}
+		if (match === true) {
+			var re = new RegExp(line,"g");
+			fs.readFile('config/friends.csv', 'utf8', function (err,data) {
+			if (err) {
+				return console.log(err);
+			}
+			var result = data.replace(re, line +' '+targetUser.userid);
+			fs.writeFile('config/friends.csv', result, 'utf8', function (err) {
+				if (err) return console.log(err);
+			});
+			});
+		} else {
+			var log = fs.createWriteStream('config/friends.csv', {'flags': 'a'});
+			log.write("\n"+user.userid+','+targetUser.userid);
+		}
+		this.sendReply(targetUser.name + ' was added to your friends list.');
+		targetUser.send(user.name + ' has added you to their friends list.');
+	},
+	
+	removefriend: function(target, room, user, connection) {
+		if(!target) return this.parse('/help removefriend');
+		var noCaps = target.toLowerCase();
+		var idFormat = toUserid(target);
+		var data = fs.readFileSync('config/friends.csv','utf8')
+		var match = false;
+		var line = '';
+		var row = (''+data).split("\n");
+		for (var i = row.length; i > -1; i--) {
+			if (!row[i]) continue;
+			var parts = row[i].split(",");
+			var userid = toUserid(parts[0]);
+			if (user.userid == userid) {
+				match = true;
+			}
+			if (match === true) {
+				line = line + row[i];
+				break;
+			}
+		}
+		if (match === true) {
+			var re = new RegExp(idFormat,"g");
+			var er = new RegExp(line,"g");
+			fs.readFile('config/friends.csv', 'utf8', function (err,data) {
+			if (err) {
+				return console.log(err);
+			}
+			var result = line.replace(re, '');
+			var replace = data.replace(er, result);
+			fs.writeFile('config/friends.csv', replace, 'utf8', function (err) {
+				if (err) return console.log(err);
+			});
+			});
+		} else {
+			return this.sendReply('This user doesn\'t appear to be in your friends. Make sure you spelled their username right.');
+		}
+		this.sendReply(idFormat + ' was removed from your friends list.');
+		if(Users.get(target).connected) {
+			Users.get(target).send(user.name + ' has removed you from their friends list.');
+		}
+	},
+	
 	version: function(target, room, user) {
 		if (!this.canBroadcast()) return;
 		this.sendReplyBox('Server version: <b>'+CommandParser.package.version+'</b> <small>(<a href="http://pokemonshowdown.com/versions#' + CommandParser.serverVersion + '">' + CommandParser.serverVersion.substr(0,10) + '</a>)</small>');
