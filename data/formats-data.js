f3dc608a35b9c77597a575c72eb6e600c9708948
dtalley11 <talley_daniel@yahoo.com>
2014-03-23 00:23:40 -0400

    Added Embuzzle

--- data/formats-data.js
+++ data/formats-data.js
@@ -4716,14 +4716,21 @@ exports.BattleFormatsData = {
         tier: "Lab"
 	},
 	octoshock:{
-	viableMoves:{"hurricane":1,"brine":1,"octazooka":1,"zapcannon":1,"hydropump":1,"focusblast":1,"aquajet":1},
-	isNonstandard: true,
-	tier: "Lab"
+		viableMoves:{"hurricane":1,"brine":1,"octazooka":1,"zapcannon":1,"hydropump":1,"focusblast":1,"aquajet":1},
+		isNonstandard: true,
+		tier: "Lab"
 	},
 	thornata: {
 		viableMoves:{"calmmind":1,"growth":1,"hypervoice":1,"synthesis":1,"triattack":1,"earthpower":1,"xscissor":1,"reroot":1},
 		isNonstandard: true,
 		tier: "Lab"
+	},
+	babee: {
+        viableMoves:{
+                "electricterrain":1,
+        },
+        isNonstandard: true,
+        tier: "Lab"
 	},
 	adametal: {
         viableMoves:{"harden":1,"scratch":1,"metalclaw":1,"dragonbreath":1,"bite":1,"metalsound":1,"geargrind":1,"ironhead":1,"earthquake":1,"dragonclaw":1,"rockslide":1,"irontail":1,"outrage":1,"shadowclaw":1,"stoneedge":1},
@@ -4733,6 +4740,11 @@ exports.BattleFormatsData = {
 	bombee: {
         viableMoves:{"electricterrain":1},
         isNonstandard: true,
-        tier: "Embuzzle Playtest"
+        tier: "Lab"
 	},
+	embuzzle: {
+        viableMoves:{"electricterrain":1},
+        isNonstandard: true,
+        tier: "Lab"
+	}
 };
